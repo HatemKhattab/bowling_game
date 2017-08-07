cc1e4074a990d85df1725f0effb58ee6f6ba1981
Hatem Khattab <hattemkhattab@gmail.com>
2017-08-07 15:37:44 +0200

    calculate score when two strike adjasent - add ball3 to frame10 if strike or spare

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,14 +9,15 @@ class Game < ActiveRecord::Base
     #TODO: raise error if b1 or b2 > 10 or b1+b2>10
     #TODO: ensure max 10 frames game over
     #TODO: fix score calculation
+
     ball1 = b1.present? ? b1 : rand(0..PINS)
     ball2 = b2.present? ? b2 : rand(0..PINS - ball1)
     ball2 = nil if ball1==10
     sum = ball1 + ( ball2.present? ? ball2 : 0)
     strike_status = (ball1==10)
     spare_status = ((ball1 + (ball2.present? ? ball2 : 0) ==10) && ball1 != 10)
-
-    frame = {ball_1: ball1, ball_2: ball2, score: sum , strike: strike_status, spare: spare_status }
+    ball3 = (frames.size==9 && (strike_status || spare_status) )? rand(0..10): nil
+    frame = {ball_1: ball1, ball_2: ball2,ball_3: ball3, score: sum , strike: strike_status, spare: spare_status }
     frames << frame
     clac_score(current_index) if current_index > 0
   end
@@ -29,6 +30,17 @@ class Game < ActiveRecord::Base
       frames[frame_num - 1][:score]+= current_frame[:ball_1] + (prev_frame[:strike] && !current_frame[:strike] ? current_frame[:ball_2] : 0)
     end
     frames[frame_num][:score] += prev_frame[:score]
+
+    #try code ..............................
+    if current_index >=2
+      p_prev_frame = frames[frame_num - 2]
+      if prev_frame[:strike] && p_prev_frame[:strike]
+        # add current ball_1 to p_prev, prev and current score
+        (frame_num-2 .. frame_num).each do |num|
+        frames[num][:score] += current_frame[:ball_1]
+        end
+      end
+    end
   end
 
     def current_index
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -38,11 +38,18 @@ RSpec.describe Game, type: :model do
     expect(frame[:ball_2]).to be_nil
   end
 
-  describe 'Testing with existing scenario' do
+  it " Add ball3 in the tenth frame if it was strike or spare" do
+    (0..9).each do |num|
+      @game.play(10,nil)
+    end
+    expect(@game.frames[9][:ball_3]).to be_between(0, 10)
+  end
+
+  describe 'Testing with scenario 1' do
     # game table at http://www.alltombowling.nu/skola_rakna.php
     before {
       @game = Game.new
-      frames = [[6, 2], [8, 2], [10, nil], [9, 0], [6, 4], [8, 1], [9, 1], [10, nil], [10, nil]]
+      frames = [[6, 2], [8, 2], [10, nil], [9, 0], [6, 4], [8, 1], [9, 1], [10, nil], [10, nil], [8, 2]]
       frames.each do |b1, b2|
         @game.play(b1, b2)
       end
@@ -56,6 +63,10 @@ RSpec.describe Game, type: :model do
       frame_5 = @game.frames[4]
       frame_6 = @game.frames[5]
       frame_7 = @game.frames[6]
+      frame_8 = @game.frames[7]
+      frame_9 = @game.frames[8]
+      frame_10 = @game.frames[9]
+
       expect(frame_1[:score]).to eq(8)
       expect(frame_2[:score]).to eq(28)
       expect(frame_3[:score]).to eq(47)
@@ -63,7 +74,32 @@ RSpec.describe Game, type: :model do
       expect(frame_5[:score]).to eq(74)
       expect(frame_6[:score]).to eq(83)
       expect(frame_7[:score]).to eq(103)
+      expect(frame_8[:score]).to eq(131)
+      expect(frame_9[:score]).to eq(151)
+      expect(frame_10[:score]).to eq(161)
     end
+  end
 
+  describe 'Testing with scenario 1' do
+
+    before {
+      @game = Game.new
+      frames = [[10, nil], [10, nil], [10, nil], [5, 1]]
+      frames.each do |b1, b2|
+        @game.play(b1, b2)
+      end
+    }
+
+    it 'calculate the right score' do
+      frame_1 = @game.frames[0]
+      frame_2 = @game.frames[1]
+      frame_3 = @game.frames[2]
+      frame_4 = @game.frames[3]
+
+      expect(frame_1[:score]).to eq(30)
+      expect(frame_2[:score]).to eq(55)
+      expect(frame_3[:score]).to eq(71)
+      expect(frame_4[:score]).to eq(77)
+    end
   end
 end
