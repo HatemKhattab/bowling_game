973aba7f540308d1de8f2a8e3155524b0a9408e9
Hatem Khattab <hattemkhattab@gmail.com>
2017-08-15 12:16:31 +0200

    page contains two buttons

--- spec/features/game_features_spec.rb
+++ spec/features/game_features_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 describe 'Bowling features', :type => :feature do
+  before {
+    visit '/'
+  }
+
+  it 'index page contains two buttons' do
+    expect(page).to have_button('Player 1')
+    expect(page).to have_button('Player 2')
+  end
 
 end
 
