595adffcc25d32d5db19759d0ffd41c9fb068356
Hatem Khattab <hattemkhattab@gmail.com>
2017-08-16 13:38:44 +0200

    fix disable button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,19 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:index, :play1, :play2]
-
+  before_action :game_end, only: :index
   def index
-    #session[:player] = 1 if session[:player].nil?
+    session[:player] = 1 if session[:player].nil?
   end
 
   def play1
     @game_1.play
+    session[:player] = 2
     redirect_to action: 'index'
   end
 
   def play2
     @game_2.play
+    session[:player] = 1
     redirect_to action: 'index'
   end
 
@@ -21,6 +23,8 @@ class GamesController < ApplicationController
         game.destroy
       end
     end
+    session[:game_over] = false
+    session[:info] = nil
     redirect_to action: 'index'
   end
 
@@ -30,5 +34,14 @@ class GamesController < ApplicationController
     player_2 = Player.find_by_name('player2')
     @game_1 = player_1.game.nil? ? player_1.create_game : player_1.game
     @game_2 = player_2.game.nil? ? player_2.create_game : player_2.game
+  end
+
+  def game_end
+    if @game_1.over? && @game_2.over?
+      session[:game_over] = true
+      score_1 = @game_1.frames[9][:score]
+      score_2 = @game_2.frames[9][:score]
+      session[:info] = 'Game over !!'
+    end
   end
 end
